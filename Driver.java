d1494bccfd1ae0e205b4408b2a69b78ba7c4daff
alisnwu <alisonwu1020@gmail.com>
2019-05-10 21:45:41 -0400

    project

---
+++ Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+  public static void main(String[] args){
+    String[] arr = new String[]{"astro", "yeah", "that's", "how", "we", "already", "know", "winter's", "here"};
+
+  }
+}
