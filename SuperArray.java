dcdee16d68c094cbbb48c955464661a63ecb74e5
alisnwu <alisonwu1020@gmail.com>
2019-05-18 20:48:19 -0400

    hw

--- SuperArray.java
+++ SuperArray.java
@@ -1,142 +1,123 @@
 public class SuperArray{
   private String[] arr;
   private int size;
-  public SuperArray(){
-    arr = new String[10];
+  public SuperArray(String[] array){
+    arr = array;
+    size = array.length;
+  }
+  public String toString(){
+    String arr1 = "";
+    for(int i = 0; i < size; i ++){
+      arr1 = arr1.concat(arr[i]  +",");
+    }
+    return "["+ arr1 + "]";
+  }
+  public void resize(int pivot){
+    String[] arr2 = new String[size + 1];
+    size ++;
+    for(int i = 0; i < pivot; i ++){
+      arr2[i] = arr[i];
+    }
+    for(int i = pivot; i < size; i ++){
+      arr2[i] = arr[i - 1];
+    }
+    arr = new String[size];
+    arr = arr2;
+  }
+  public void cut(int pivot){
+    String[] arr2 = new String[size - 1];
+    size --;
+    for(int i = 0; i < pivot; i ++){
+      arr2[i] = arr[i];
+    }
+    for(int i = pivot; i < size; i ++){
+      arr2[i] = arr[i + 1];
+    }
+    arr = new String[size];
+    arr = arr2;
   }
   public void clear(){
-    arr = new String[0];
+    arr = new String[size];
   }
   public int size(){
-    int counter = 0;
-    for(int i = 0; i < arr.length; i++){
-      if(arr[i] != null){
-        counter++;
-      }
-    }
-    return counter;
+    return size;
   }
   public boolean isEmpty(){
-    int counter = 0;
-    for(int i = 0; i < arr.length; i++){
+    for(int i = 0; i < size; i ++){
       if(arr[i] != null){
-        counter++;
+        return false;
       }
     }
-    if(counter == 0){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-  public void resize(){
-    String[] arr1 = new String[arr.length + 1];
-    for(int i = 0; i < arr.length; i++){
-      arr1[i] = arr[i];
-    }
-    String[] arr = new String[arr1.length];
-    for(int j = 0; j < arr1.length; j++){
-      arr[j] = arr1[j];
-    }
+    return true;
   }
   public boolean add(String element){
-    if(arr[arr.length - 1] == null){
-      for(int i = arr.length - 1; i >= 0; i--){
-        if(arr[i] != null){
-          arr[i+1] = element;
-          return true;
-        }
-      }
-    }
-    this.resize();
-    arr[arr.length - 1] = element;
+    resize(size);
+    arr[size - 1] = element;
     return true;
   }
   public String get(int index){
     try{
       return arr[index];
-      }
-    catch (ArrayIndexOutOfBoundsException e){
+    }
+    catch(ArrayIndexOutOfBoundsException e){
       return("array index out of bounds");
     }
   }
   public String set(int index, String element){
-    String st = "";
+    String old = arr[index];
     try{
-      st = arr[index];
       arr[index] = element;
     }
-    catch (ArrayIndexOutOfBoundsException e){
-      System.out.println(e.getMessage());
+    catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("array index out of bounds");
     }
-    return st;
-  }
-  public String toString(){
-    String format = "[";
-    for(int i=0; i<arr.length;i++){
-      format = format + arr[i];
-      if(i < arr.length - 1){
-          format = format + ",";
-      }
-    }
-    return format;
+    return old;
   }
   public boolean contains(String element){
-    for(int i = 0; i < arr.length; i++){
-      if(arr[i] == element){
+    for(int i = 0; i < size; i ++){
+      if(arr[i].equals(element)){
         return true;
       }
     }
     return false;
   }
   public void add(int index, String element){
-    try{
-      for(int i = arr.length - 1; i > index; i--){
-        arr[i] = arr[i-1];
-      }
-      arr[index] = element;
+    if(index > size){
+      System.out.println("index out of bounds");
+      return;
     }
-    catch(ArrayIndexOutOfBoundsException e){
-      System.out.println(e.getMessage());
-    }
+    resize(index);
+    arr[index] = element;
   }
-  public int indexOf(String element){
-    for(int i = 0; i < arr.length; i++){
-      if(arr[i] == element){
+  public int IndexOf(String element){
+    for(int i = 0; i < size; i ++){
+      if(element == arr[i]){
         return i;
       }
     }
     return -1;
   }
   public int lastIndexOf(String element){
-    for(int i = arr.length - 1; i >= 0; i--){
-      if(arr[i] == element){
+    for(int i = size-1; i >= 0; i --){
+      if(element == arr[i]){
         return i;
       }
     }
     return -1;
   }
   public String remove(int index){
-    String str ="";
-    try{
-      str = arr[index];
-      for(int i = index; i < arr.length; i++){
-        arr[i] = arr[i+1];
-      }
+    if(index >= size){
+      System.out.println("array index out of bounds");
+      System.exit(0);
     }
-    catch(ArrayIndexOutOfBoundsException e){
-      System.out.println("this is the error for remove 1: " + e.getMessage());
-    }
-    return str;
+    String removed = arr[index];
+    cut(index);
+    return removed;
   }
   public boolean remove(String element){
-    for(int i = 0; i < arr.length; i++){
+    for(int i = 0; i < size; i ++){
       if(arr[i] == element){
-        for(int j = i; j < arr.length - 1; j++){
-          arr[j] = arr[j+1];
-        }
-        arr[arr.length - 1] = null;
+        cut(i);
         return true;
       }
     }
